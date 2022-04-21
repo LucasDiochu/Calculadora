b9e09cd2f4d3358be305daa21b0aa6f6f8dc5618
Lucas <lucasbaneves@gmail.com>
2022-04-21 20:14:12 -0300

    Adicionar arquivos de projeto.

---
+++ Views/CalculadoraWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Calculadora.ViewsModel;
+
+namespace Calculadora.Views
+{
+    /// <summary>
+    /// Lógica interna para CalculadoraWindow.xaml
+    /// </summary>
+    public partial class CalculadoraWindow : Window
+    {
+        public CalculadoraWindow()
+        {
+            InitializeComponent();
+            DataContext = new CalculadoraViewsModel();
+        }
+    }
+}
---
+++ ViewsModel/CalculadoraViewsModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace Calculadora.ViewsModel
+{
+    public class CalculadoraViewsModel : BindableBase
+    {
+        public CalculadoraViewsModel()
+        {
+            SomaCommand = new DelegateCommand(Soma);
+            SubtracaoCommand = new DelegateCommand(Subtracao);
+            MultiplicacaoCommand = new DelegateCommand(Multiplicacao);
+            DivisaoCommand = new DelegateCommand(Divisao);
+        }
+        
+        public int Num1 { get; set; }
+        
+        public int Num2 { get; set; }
+
+        public int Resultado { get; set; }
+
+        public DelegateCommand SomaCommand { get; set; }
+
+        public DelegateCommand SubtracaoCommand { get; set; }
+
+        public DelegateCommand MultiplicacaoCommand { get; set; }
+
+        public DelegateCommand DivisaoCommand { get; set; }
+
+        public void Soma()
+        {
+            Resultado = Num1 + Num2;
+        }
+
+        public void Subtracao()
+        {
+            Resultado = Num1 - Num2;
+        }
+
+        public void Multiplicacao()
+        {
+            Resultado = Num1 * Num2;
+        }
+
+        public void Divisao()
+        {
+            Resultado = Num1 / Num2;
+        }
+
+    }
+}
